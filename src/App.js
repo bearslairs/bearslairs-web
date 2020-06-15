df54a9a3e4a8fa02e4711a3d66b14fa87d75c498
Rob Thijssen <rthijssen@gmail.com>
2020-06-15 12:44:43 +0300

    layout complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { LinkContainer } from 'react-router-bootstrap'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -20,8 +20,9 @@ const CopyApi = 'https://raw.githubusercontent.com/bearslairs/bearslairs-data/ma
 const UbiBotApi = 'https://api.ubibot.io/channels/13604?api_key=609210eb2306427a88d662d48ddb578d';
 const languages = ['bg', 'en', 'ru'];
 const lairImages = ['/lair-baby.png', '/lair-mama.png', '/lair-papa.png'];
+const bikeImages = ['/bike-rack.png', '/bike-small.png', '/bike-large.png'];
 
-class App extends React.Component {
+class App extends Component {
   state = {
     language: 'en',
     copy: {
@@ -151,46 +152,47 @@ class App extends React.Component {
         </Row>
       </Container>
       <Container id="container-about" fluid>
-        <Container>
-          <Row className="justify-content-xl-center justify-content-md-center">
-            <Col xl={3}>
-              <Image src={'/about-access.png'} />
-              <p>
-                when you book and pay for your locker, a unique door access code is created just for you. each customer gets a different access code that will work during daytime hours for the duration of the locker booking.
-              </p>
-            </Col>
-            <Col xl={3}>
-              <Image src={'/about-notify.png'} />
-              <p>
-                every time your code is used to access the storage facility, we will send you an email so that you are aware of anyone accessing your locker, if you have shared your door access code with family or trusted friends.
-              </p>
-            </Col>
-            <Col xl={3}>
-              <Image src={'/about-security.png'} />
-              <p>
-                individual lockers are secured with your own padlock. only you can decide who will have copies of your locker key and will have access to your secure locker. we do not keep copies of keys for booked lockers. if your keys are lost or stolen, we can remove the lock for a fee of bgn 20.00 but the lock will be destroyed.
-              </p>
-            </Col>
-            <Col xl={3}>
-              <Image src={'/about-time.png'} />
-              <p>
-                you may access your locker between the hours of 8:00 and 22:00. please be considerate of the neighbours and do not make excessive noise if you need access to your locker after sunset and before sunrise. night access is restricted in consideration of our residential neighbours.
-              </p>
-            </Col>
-          </Row>
-        </Container>
+        {
+          this.state.copy.blurbs.slice(0, 1).map((blurb, blurbIndex) => (
+            <Container key={blurbIndex}>
+              <h3>{blurb.title}</h3>
+              <Row className="justify-content-xl-center justify-content-md-center">
+              {
+                blurb.copy.map((paragraph, paragraphIndex) => (
+                  <Col xl={(12 / blurb.copy.length)} key={paragraphIndex}>
+                    <Image src={paragraph.image} />
+                    <p>{paragraph.text}</p>
+                  </Col>
+                ))
+              }
+              </Row>
+            </Container>
+          ))
+        }
       </Container>
       <Container id="container-security" fluid>
-        <Container style={{width: '50%'}}>
-          <h3>
-            secure, private lockers
-          </h3>
-          <p>
-            our secure, private lockers give you complete peace of mind for your belongings.<br />
-            the facility is monitored 24 hours a day by a comprehensive cctv system.<br />
-            choose from our baby (small), mama (medium) and papa (large) bears lairs.<br />
-            each locker is secured with a lock that only you have keys for.
-          </p>
+        <Container>
+          <Row className="justify-content-xl-center justify-content-md-center">
+            {
+              this.state.copy.blurbs.slice(1, 2).map((blurb, blurbIndex) => (
+                <Col xl={7} key={blurbIndex}>
+                  <h3>{blurb.title}</h3>
+                  {
+                    blurb.copy.map((paragraph, paragraphIndex) => (
+                      <p key={paragraphIndex}>
+                        {paragraph}
+                      </p>
+                    ))
+                  }
+                  <LinkContainer to={'/book'}>
+                    <Button size="sm">
+                      book now
+                    </Button>
+                  </LinkContainer>
+                </Col>
+              ))
+            }
+          </Row>
         </Container>
       </Container>
       <Container id="container-lair">
@@ -211,13 +213,12 @@ class App extends React.Component {
                     {card.description.join(' ')}
                   </Card.Title>
                   <hr />
-
-                  <ul>
+                  <ul style={{lmargin: 0, padding: 0}}>
                     {
                       card.features.map((feature, featureIndex) => (
                         <li key={featureIndex} style={{fontWeight: 600, margin: 0, padding: 0}}>
                           {feature.text}
-                          <ul>
+                          <ul style={{lmargin: 0, padding: 0}}>
                             {
                               feature.details.map((detail, detailIndex) => (
                                 <li key={detailIndex} style={{listStyleType: 'none', fontWeight: 'normal', margin: 0, padding: 0}}>
@@ -243,6 +244,150 @@ class App extends React.Component {
             ))
           }
         </Row>
+      </Container>
+      <Container id="container-adaptive" fluid>
+        {
+          this.state.copy.blurbs.slice(2, 3).map((blurb, blurbIndex) => (
+            <Container key={blurbIndex}>
+              <h3>{blurb.title}</h3>
+              <Row className="justify-content-xl-center justify-content-md-center">
+                <Col xl={7}>
+                  <p>{blurb.copy[0]}</p>
+                </Col>
+              </Row>
+              <blockquote className="blockquote">
+                <p>{blurb.copy[1]}</p>
+              </blockquote>
+            </Container>
+          ))
+        }
+      </Container>
+      <Container id="container-bike" fluid>
+        {
+          this.state.copy.blurbs.slice(3, 4).map((blurb, blurbIndex) => (
+            <Container key={blurbIndex}>
+              <h3>{blurb.title}</h3>
+              <Row className="justify-content-xl-center justify-content-md-center">
+                <Col xl={8}>
+                  <p>{blurb.copy[0]}</p>
+                </Col>
+              </Row>
+            </Container>
+          ))
+        }
+        <Container>
+          <Row className="justify-content-xl-center justify-content-md-center">
+            {
+              this.state.copy.cards.slice(3, 6).map((card, cardIndex) => (
+                <Col key={cardIndex}>
+                <Card className="h-100">
+                  <Card.Header>
+                    {card.title}
+                  </Card.Header>
+                  <Card.Img variant="top" src={bikeImages[cardIndex]} alt={card.image.alt} rounded="true" />
+                  <Card.Body>
+                    <Card.Title>
+                      {card.description.join(' ')}
+                    </Card.Title>
+                    <hr />
+                    <ul style={{lmargin: 0, padding: 0}}>
+                      {
+                        card.features.map((feature, featureIndex) => (
+                          <li key={featureIndex} style={{fontWeight: 600, margin: 0, padding: 0}}>
+                            {feature.text}
+                            <ul style={{margin: 0, padding: 0}}>
+                              {
+                                feature.details.map((detail, detailIndex) => (
+                                  <li key={detailIndex} style={{listStyleType: 'none', fontWeight: 'normal', margin: 0, padding: 0}}>
+                                    {detail}
+                                  </li>
+                                ))
+                              }
+                            </ul>
+                          </li>
+                        ))
+                      }
+                    </ul>
+                  </Card.Body>
+                  <Card.Footer>
+                    <LinkContainer to={card.button.link}>
+                      <Button size="sm">
+                        {card.button.text}
+                      </Button>
+                    </LinkContainer>
+                  </Card.Footer>
+                </Card>
+                </Col>
+              ))
+            }
+          </Row>
+        </Container>
+      </Container>
+      <Container id="container-facility" fluid>
+        {
+          this.state.copy.blurbs.slice(4, 5).map((blurb, blurbIndex) => (
+            <Container key={blurbIndex}>
+              <h3>{blurb.title}</h3>
+              <Row className="justify-content-xl-center justify-content-md-center">
+              {
+                blurb.copy.map((paragraph, paragraphIndex) => (
+                  <Col xl={(12 / blurb.copy.length)} key={paragraphIndex}>
+                    <Image src={paragraph.image} />
+                    <p>{paragraph.text}</p>
+                  </Col>
+                ))
+              }
+              </Row>
+              <Row className="justify-content-xl-center justify-content-md-center">
+                <Col xl="auto" md="auto">
+                  <LinkContainer to={'/book'}>
+                    <Button size="sm">
+                      book now
+                    </Button>
+                  </LinkContainer>
+                </Col>
+              </Row>
+            </Container>
+          ))
+        }
+      </Container>
+      <Container id="container-map" fluid>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCKw8by28pNI5tlimezyyjgtXz_Nvkq2-Y' }}
+          defaultCenter={{ lat: 41.820582, lng: 23.478257 }}
+          defaultZoom={ 14 }
+          onGoogleApiLoaded={({map, maps}) => {
+            let marker = new maps.Marker({
+              position: { lat: 41.820582, lng: 23.478257 },
+              map,
+              title: 'Bears Lairs, Bansko',
+              description: 'secure, self-storage in bansko with access at your convenience',
+              link: {
+                url: 'https://www.google.com/maps/place/Bears+Lairs/@41.8223813,23.4681867,15z/data=!4m5!3m4!1s0x0:0xadd4ea4c0b9a3216!8m2!3d41.820631!4d23.478215',
+                text: 'maps.google.com/Bears+Lairs'
+              }
+            });
+            let infoWindow = new maps.InfoWindow({
+              content: '<h4><img src="favicon-32x32.png" style="margin-right: 6px;" class="rounded-circle" />' + marker.title + '</h4><p>' + marker.description + '<br /><a href="' + marker.link.url + '">' + marker.link.text + '</a></p>'
+            });
+            infoWindow.open(map, marker);
+            marker.addListener('click', () => {
+              map.setZoom(14);
+              map.setCenter(marker.getPosition());
+              infoWindow.open(map, marker);
+            });
+          }} />
+      </Container>
+      <Container id="container-footer" fluid>
+        <Container>
+          <Row className="justify-content-xl-center justify-content-md-center">
+            <Col xl={6}>
+              <a href="mailto:enquiries@bearslairs.eu">enquiries@bearslairs.eu</a>
+              <hr />
+              &copy; 2020, Bears Lairs EOOD, Bulgaria. All rights reserved
+            </Col>
+          </Row>
+        </Container>
       </Container>
     </>
     );
